0070843991c043e2b7f967f4cd8bcc51599b8fbf
gin <gin-18@qq.com>
2023-10-30 00:11:18 +0800

    update dwm_battery

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,16 +4,17 @@
 arr=("dunst" "fcitx5" "picom")
 
 for value in ${arr[@]}; do
-    if [[ ! $(pgrep ${value}) ]]; then
-        exec "$value" &
-    fi
+  if [[ ! $(pgrep ${value}) ]]; then
+    exec "$value" &
+  fi
 done
 
+xrdb merge ~/.Xresources
+xinput --set-prop 11 "libinput Tapping Enabled" 1
 /bin/bash ~/suckless/dwm/scripts/dwm-wallpaper.sh &
-xinput --set-prop 12 "libinput Tapping Enabled" 1
 
 # 状态栏的刷新
 while true; do
-	/bin/bash ./dwm-status-refresh.sh
-	sleep 1
+  /bin/bash ./dwm-status-refresh.sh
+  sleep 1
 done
